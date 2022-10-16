e41ca936b0b8216f0df431a9fe993111bf13658e
Juan-Gines <JuangAlenta@hotmail.com>
2022-10-16 10:36:51 +0200

    Sesiones 26 27 ejemplos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter';
+import LoginUseState from './components/LoginUseState';
+import LoginUseReducer from './components/LoginUseReducer';
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<h1>useReducer Examples - Alternative to Redux</h1>
-				<Counter></Counter>
+				{/* <h1>useReducer Examples - Alternative to Redux</h1> */}
+				{/* <Counter></Counter> */}
+				<h1>useReducer VS useState</h1>
+				{/* <LoginUseState></LoginUseState> */}
+				<LoginUseReducer></LoginUseReducer>
 			</header>
 		</div>
 	);
